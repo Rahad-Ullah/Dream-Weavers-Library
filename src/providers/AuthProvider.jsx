02e3056967e6de657cd2b682ba0faa20de4b3e63
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-11-07 07:13:48 +0600

    Firebase authentication has been initialized for a better authentication experience

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,54 @@
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from '../firebase/firebase.config'
 
-const AuthProvider = () => {
+
+export const AuthContext = createContext()
+const auth = getAuth(app)
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe =  onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+            // if user exist then issue a token
+        })
+        return () => {
+            return unsubscribe();
+        }
+    } ,[])
+
+
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        signIn,
+        logOut
+    }
+    
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider >
     );
 };
 

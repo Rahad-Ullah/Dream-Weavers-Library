e5a987dcc610747e0a7e147f4def88f55debe00e
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-11-08 15:05:40 +0600

    Adding to Borrowed Collection has been implemented successfully

--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -1,11 +1,44 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { Rating } from 'primereact/rating';
-
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import toast, { Toaster } from 'react-hot-toast';
+        
 
 const BookDetails = () => {
     const book = useLoaderData()
     const {_id, name, author_name, image, category, rating, quantity} = book;
+    const {user} = useAuth()
+
+    const handleBorrow = (e) => {
+        e.preventDefault()
+        const form = e.target;
+        const userName = form.name.value;
+        const email = form.email.value;
+        const returnDate = form.return_date.value;
+        const today = new Date()
+        const borrowedDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        const borrowOrder = {
+            userName,
+            email,
+            name,
+            image,
+            category,
+            borrowedDate,
+            returnDate
+        }
+        
+        axios.post(`http://localhost:5000/borrowed-books`, borrowOrder)
+        .then((result) => {
+            console.log(result.data)
+            toast.success('Added to borrowed list.')
+        }).catch((err) => {
+            console.log(err)
+            toast.error('Faild to add borrowed list.')
+        });
+    }
     
+
     return (
         <div className="flex flex-col lg:flex-row gap-12 max-w-6xl mx-auto py-16 px-4 md:px-6 lg:px-8 font-poppins">
             <div className="w-full lg:w-1/4 flex justify-center">
@@ -21,9 +54,66 @@ const BookDetails = () => {
                 </div>
                 <div className="flex gap-6 pt-4">
                     <Link to={`/pdf-view/${_id}`} className="btn btn-primary normal-case text-base btn-outline">Read Now</Link>
-                    <Link className="btn btn-primary normal-case text-base">Borrow</Link>
+                    <Link className="btn btn-primary normal-case text-base" onClick={()=>document.getElementById('borrow_modal').showModal()}>Borrow</Link>
                 </div>
             </div>
+            
+            {/* Dialogue Modal */}
+            <dialog id="borrow_modal" className="modal modal-middle sm:modal-middle">
+                <div className="modal-box w-10/12 mx-auto">
+                    <form method="dialog">
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-4 top-4">✕</button>
+                    </form>
+                    {/*  */}
+                    <h2 className="text-3xl font-semibold text-accent text-center mb-2">Borrow Book</h2>
+                    <form onSubmit={handleBorrow} className="card-body py-2 px-0 md:px-4">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold">Name</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Your name"
+                            className="input input-bordered"
+                            name="name"
+                            defaultValue={user?.name}
+                            required
+                        />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold">Email</span>
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Your email"
+                            className="input input-bordered"
+                            name="email"
+                            defaultValue={user?.email}
+                            required
+                        />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text font-semibold">Return Date</span>
+                        </label>
+                        <input
+                            type="date"
+                            placeholder="Return date"
+                            className="input input-bordered"
+                            name="return_date"
+                            required
+                        />
+                        </div>
+                        <div className="form-control mt-6 flex-row justify-center">
+                            <button className="btn btn-primary normal-case text-base">Submit</button>
+                        </div>
+                    </form>
+                    {/*  */}
+                </div>
+                <Toaster position="top-center"/>
+            </dialog>
         </div>
     );
 };
